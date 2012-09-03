6d265492c4a5d6df839ba19c417c11067010d7a5
Rob Howard <rob@robhoward.id.au>
2012-09-04 06:05:14 +1000

    Adds Rails skeleton to get Konacha tests going.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,18 @@
-source :rubygems
+source 'https://rubygems.org'
 
-gem 'zurb-foundation', '> 3.0.0'
-gem 'compass'
+gem 'rails', '3.2.8'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'zurb-foundation', '> 3.0.0'
+  gem 'compass'
+
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  # gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Booth::Application.config.secret_token = 'c8e990e765aeb4e3a4dcedb7d1f2d12c8065274055f5804d0a5e13a8c687d05dde8a3fbcaf7c3eadcc0cb830ce24ff14aa8592334cb252f5007bce3ed063586b'
