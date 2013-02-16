265b94d2c3c77edf8a8c311a69e5e0e9e223fb41
Rob Howard <rob@robhoward.id.au>
2013-02-17 03:29:13 +1100

    Adds JSON support for /events.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,14 @@
 class EventsController < ApplicationController
+  respond_to :json, :html
+
   def index
     @events = events.all
+    respond_with(@events)
   end
 
   def show
     @event = events.find(params[:id])
+    respond_with(@event)
   end
 
   protected
