6b43dac0ecd128de986a34c7c927628de57f0f2e
Rob Howard <rob@robhoward.id.au>
2013-04-21 03:27:36 +1000

    Adds state machine to order, modelling transitions between new, owing/owed and resolved. (Cancellation is still a TODO.)

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,8 +4,85 @@ class Order < ActiveRecord::Base
 
   ### Validations ###
   validates :email, :presence => true, :email => true
-  validates :line_items, :length => {:minimum => 1}
+  validates :line_items, :length => {
+    :minimum => 1,
+    :message => "need at least one item in the order",
+  }
 
   ### Associations ###
   has_many :line_items, :validate => true, :inverse_of => :order
+
+  ### State Machine ###
+  state_machine :status, :initial => :new do
+    ACTIVE = [:resolved, :payment_pending, :refund_pending]
+
+    state :new
+    state :payment_pending
+    state :refund_pending
+    state :resolved
+    state :cancelled
+
+    # Events
+    event :confirm do
+      transition :new => :resolved, :if => :free?
+      transition :new => :payment_pending, :if => :needs_payment?
+    end
+
+    event :resolve do
+      transition ACTIVE => :payment_pending, :if => :needs_payment?
+      transition ACTIVE => :refund_pending, :if => :needs_refund?
+      transition ACTIVE => :resolved
+    end
+
+    # TODO: Hook that cancels attached line-items.
+    #event :cancel do
+    #  transition ACTIVE => :refund_pending, :if => :will_need_refund?
+    #  transition ACTIVE => :cancelled
+    #end
+
+    # TODO: 'reopen' even not needed right now; add this plus after_save hooks on
+    #       Ticket models to trigger reopening (if can_reopen?)
+    #event :reopen do
+    #  transition :cancelled => :payment_pending, :if => :needs_payment?
+    #  transition :cancelled => :resolved, :if => :free?
+    #end
+
+    # TODO: Transition hooks; are these necessary for auditing?
+    #after_transition any - :resolved => :resolved, :do => :update_resolved_at
+    #after_transition :resolved => any - :resolved, :do => :update_resolved_at
+  end
+
+
+  ### Pricing ###
+
+  def free?
+    total_price == 0
+  end
+
+  def total_price
+    line_items.map(&:amount).reduce(0, :+)
+  end
+
+
+  ### Balance/Transactions ###
+
+  def balance
+    0 # TODO: transactions.sum(:amount)
+  end
+
+  def settled_balance?
+    !(needs_payment? && needs_refund?)
+  end
+
+  def needs_payment?
+    balance < total_price
+  end
+
+  def needs_refund?
+    total_price < balance
+  end
+
+  def will_need_refund?
+    balance > 0
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,27 +4,134 @@ describe Order do
   context "validation" do
     context "email" do
       it "accepts a valid email" do
-        FactoryGirl.build(:order, :with_line_items, :email => 'me@example.com')
-                   .should be_valid
+        order(:email => 'me@example.com').should be_valid
       end
       it "requires an email" do
-        FactoryGirl.build(:order, :with_line_items, :email => nil)
-                   .should_not be_valid
+        order(:email => nil).should_not be_valid
       end
       it "rejects an valid email" do
-        FactoryGirl.build(:order, :with_line_items, :email => 'example.com')
-                   .should_not be_valid
+        order(:email => 'example.com').should_not be_valid
       end
     end
 
     context "line_items" do
       it "needs at least one line item" do
-        order = FactoryGirl.build(:order)
+        order = FactoryGirl.build(:order, :line_items => [])
         order.should_not be_valid
 
         order.line_items << FactoryGirl.build(:line_item, :order => order)
         order.should be_valid
       end
     end
+  end
+
+  context "pricing" do
+    it "treats orders adding to $0.00 to be free" do
+      order = FactoryGirl.build(:order, :line_items => [
+                FactoryGirl.build(:line_item, :price => 0)
+              ])
+      order.total_price.should == 0
+      order.should be_free
+    end
+    it "adds the quantity * price of all tickets to calculate the total_price" do
+      order = FactoryGirl.build(:order, :line_items => [
+                FactoryGirl.build(:line_item, :price => 10),
+                FactoryGirl.build(:line_item, :price => 20),
+              ])
+      order.total_price.should == 30
+      order.should_not be_free
+    end
+  end
+
+  context "state" do
+    it "has an initial state" do
+      order.should be_new
+    end
+    it "won't transition if the order is invalid" do
+      order = FactoryGirl.build(:order)
+      -> { order.confirm! }.should raise_error(StateMachine::InvalidTransition)
+    end
+    it "will save itself when transitioning" do
+      order = FactoryGirl.build(:order, :with_line_items)
+      order.confirm!
+      order.should be_persisted
+    end
+    it "can immediately be resolved if the total price is nothing" do
+      order = FactoryGirl.build(:order, :line_items => [
+                FactoryGirl.build(:line_item, :price => 0),
+              ])
+      order.confirm!
+      order.should be_free
+      order.should be_resolved
+    end
+    it "will be payment-pending if owing" do
+      order = FactoryGirl.build(:order, :line_items => [
+                FactoryGirl.build(:line_item, :price => 10),
+              ])
+      order.confirm!
+      order.needs_payment?.should be_true
+      order.should be_payment_pending
+    end
+    it "will be payment-pending if still owing after payment" do
+      order = FactoryGirl.build(:order, :line_items => [
+                FactoryGirl.build(:line_item, :price => 10),
+              ])
+      order.confirm!
+
+      order.stub(:balance => 5)
+      order.resolve!
+      order.needs_payment?.should be_true
+      order.should be_payment_pending
+    end
+    it "will be refunding-pending if owed after payment" do
+      order = FactoryGirl.build(:order, :line_items => [
+                FactoryGirl.build(:line_item, :price => 10),
+              ])
+      order.confirm!
+
+      order.stub(:balance => 15)
+      order.resolve!
+      order.needs_payment?.should be_false
+      order.needs_refund?.should be_true
+      order.should be_refund_pending
+    end
+    it "will be resolved if payment matches total price" do
+      order = FactoryGirl.build(:order, :line_items => [
+                FactoryGirl.build(:line_item, :price => 10),
+              ])
+      order.confirm!
+
+      order.stub(:balance => 10)
+      order.resolve!
+      order.needs_payment?.should be_false
+      order.needs_refund?.should be_false
+      order.should be_resolved
+    end
+#    it "will successfully cancel if a refund will not be needed" do
+#      order = FactoryGirl.build(:order, :line_items => [
+#                FactoryGirl.build(:line_item, :price => 10),
+#              ])
+#      order.confirm!
+#
+#      order.stub(:balance => 0)
+#      order.cancel!
+#      order.needs_refund?.should be_false
+#      order.should be_cancelled
+#    end
+#    it "will switch to refund if cancelled" do
+#      order = FactoryGirl.build(:order, :line_items => [
+#                FactoryGirl.build(:line_item, :price => 10),
+#              ])
+#      order.confirm!
+#
+#      order.stub(:balance => 10)
+#      order.cancel!
+#      order.needs_refund?.should be_true
+#      order.should be_cancelled
+#    end
+  end
+
+  def order(attr={})
+    FactoryGirl.build(:order, :with_line_items, attr)
   end
 end
